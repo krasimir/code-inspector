a254d19ff95e34d39066228508caa9cb22cfad95
Krasimir Tsonev <krasimir@outset.ws>
2020-04-03 08:32:52 +0300

    Trying to define the module types

---
+++ node/code-inspector.d.ts
@@ -0,0 +1,13 @@
+export interface NormalizedNode {
+  text: string | number | boolean;
+  type: string;
+  start?: [number | undefined, number | undefined];
+  end?: [number | undefined, number | undefined];
+  left?: string | number | boolean;
+  right?: string | number | boolean;
+  nesting?: number;
+}
+
+export default interface CodeInspector {
+  analyze(code: string): { nodes: NormalizedNode[]; scopes: NormalizedNode[] };
+}
