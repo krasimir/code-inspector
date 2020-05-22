a6c91ed5f8b2534cafedf98534543f0530b73ca4
Krasimir Tsonev <krasimir@outset.ws>
2020-05-22 14:44:23 +0300

    Tweaking the eslint integration

--- src/__tests__/graph.spec.ts
+++ src/__tests__/graph.spec.ts
@@ -1,15 +1,15 @@
-import fs from 'fs';
-import { analyze, toGraph } from '../index';
+import fs from "fs";
+import { analyze, toGraph } from "../index";
 
-const JSONFile = `${__dirname}/../../demo/d3Data.json`;
+const JSONFile = `${__dirname}/../../demo/mermaidDemo.json`;
 
 const code12 = fs
   .readFileSync(`${__dirname}/code-samples/code12.ts`)
-  .toString('utf8');
+  .toString("utf8");
 
-describe('Given the code12.tsx code sample', () => {
-  describe('when transforming the code to a graph', () => {
-    it('should return a svg image', () => {
+describe("Given the code12.tsx code sample", () => {
+  describe("when transforming the code to a graph", () => {
+    it("should return a svg image", () => {
       const result = toGraph(analyze(code12));
       fs.writeFileSync(JSONFile, JSON.stringify(result, null, 2));
     });
