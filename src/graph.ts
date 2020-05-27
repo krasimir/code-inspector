f3c70dfc8d73dc7475955f019042667b7428bad4
Krasimir Tsonev <krasimir@outset.ws>
2020-05-27 08:45:50 +0300

    Working on the graph

--- src/graph.ts
+++ src/graph.ts
@@ -1,15 +1,23 @@
 /* eslint-disable no-param-reassign, @typescript-eslint/no-use-before-define, @typescript-eslint/no-empty-function */
-import uniq from 'lodash/uniq';
-import get from 'lodash/get';
-import { NormalizedNode, Analysis } from './types';
-import { accessNode } from './utils';
-import { NODES_FUNCTION_SCOPES, NODES_DEFINING_SCOPES } from './constants';
+import uniq from "lodash/uniq";
+import get from "lodash/get";
+import { NormalizedNode, Analysis } from "./types";
+import { accessNode } from "./utils";
+import { NODES_FUNCTION_SCOPES, NODES_DEFINING_SCOPES } from "./constants";
 
 interface GraphLink {
   source: string;
   target: string;
   type?: string;
 }
+
+const SCOPES_TO_ADD: Record<string, boolean> = {
+  FunctionDeclaration: true,
+  FunctionExpression: true,
+  ClassMethod: true,
+  ObjectMethod: true,
+  ClassDeclaration: true,
+};
 
 export default function(
   analysis: Analysis
@@ -19,46 +27,39 @@ export default function(
   const nodesData: NormalizedNode[] = [];
   const linksData: GraphLink[] = [];
 
-  function verifyLinking(source: string, target: string) {
-    [source, target].forEach(key => {
-      const isNodeAddedToTheGraph = !!nodesData.find(n => n.key === key);
-      if (!isNodeAddedToTheGraph) {
-        addNodeToGraph(getNodeByKey(key));
-      }
-    });
-  }
-  function addLink(s: string, t: string, type?: string) {
-    const linkExist = linksData.find(
-      ({ source, target }) =>
-        (source === s && target === t) || (source === t && target === s)
-    );
-    if (!linkExist) {
-      const link = { source: s, target: t };
-      if (type) {
-        link.type = type;
-      }
-      linksData.push(link);
-      verifyLinking(s, t);
-    }
-  }
+  const dict: Record<string, Function> = {
+    ConditionalExpression(node: NormalizedNode) {
+      addNodeToGraph(node);
+    },
+  };
+
   function addNodeToGraph(node: NormalizedNode) {
     if (!nodesData.find(({ key }) => key === node.key)) {
       nodesData.push(node);
     }
   }
-
-  const dict: Record<string, Function> = {};
-
+  function linkNodes() {
+    for (let i = 0; i < nodesData.length; i++) {
+      const node = nodesData[i];
+      const scope = node.scopePath.split(".").pop();
+      console.log(scope);
+    }
+  }
   function process(node: NormalizedNode) {
-    if (dict[node.type]) dict[node.type](node);
+    if (dict[node.type]) {
+      dict[node.type](node);
+    } else if (SCOPES_TO_ADD[node.type]) {
+      addNodeToGraph(node);
+    }
     if (node.children) {
-      node.children.forEach(c => {
+      node.children.forEach((c) => {
         process(c);
       });
     }
   }
 
   process(tree);
+  linkNodes();
 
   return { nodes: nodesData, links: linksData };
 }
